e141176b9126dc2c53351de2f0025a2c3c348967
davalbra <davalbra@espol.edu.ec>
2022-01-22 16:49:53 -0500

    Agregando los temas

--- EstructurasDatos/src/Main/Main.java
+++ EstructurasDatos/src/Main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdatos;
+package Main;
 
 /**
  *
  * @author david
  */
-public class EstructurasDatos {
+public class Main {
 
     /**
      * @param args the command line arguments
